4e618f4d27b9e3995079a8d4e7e6c80a33c5d5b7
Peter De Genaro <pdegenaro@gmail.com>
2017-02-18 15:44:46 -0500

    basic visualizer

--- script.js
+++ script.js
@@ -1,37 +1,25 @@
-// let source = ;
-// let pTag = document.getElementsByClassName('number')[0];
-//
-// let audioCtx = new AudioContext();
-// let analyser = audioCtx.createAnalyser();
-// let audioBuffer = audioCtx.createMediaElementSource(source);
-//
-// audioBuffer.connect(analyser);
-// analyser.connect(audioCtx.destination);
-// audioBuffer.start = audioBuffer.noteOn();
-
-// let audio = document.getElementsByClassName('song')[0];
-// let context = new AudioContext();
-// let source = context.createMediaElementSource(audio);
-// let analyser = context.createAnalyser();
-// analyser.fftSize = 2048;
-//
-// source.connect(analyser);
-// analyser.connect(context.destination);
-
 window.onload = function() {
-  let ctx = new AudioContext();
-  let audio = document.getElementById('song');
-  let audioSrc = ctx.createMediaElementSource(audio);
-  let analyser = ctx.createAnalyser();
+  const ctx = new AudioContext();
+  const audio = document.getElementById('song');
+  const audioSrc = ctx.createMediaElementSource(audio);
+  const analyser = ctx.createAnalyser();
+  const canvas = document.getElementById('canvas');
+  const ctxCanvas = canvas.getContext('2d');
+  ctxCanvas.fillStyle = 'green';
+
 
   audioSrc.connect(analyser);
   analyser.connect(ctx.destination);
   analyser.fftSize = 64;
 
-  let frequencyData = new Uint8Array(analyser.frequencyBinCount);
+  const frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
   function renderFrame() {
      analyser.getByteFrequencyData(frequencyData);
+     ctxCanvas.clearRect(0, 0, 300, 300);
+     frequencyData.forEach((value, i) => {
+       ctxCanvas.fillRect(value, i*10, 10, 10);
+     });
      console.log(frequencyData);
      requestAnimationFrame(renderFrame);
   }
